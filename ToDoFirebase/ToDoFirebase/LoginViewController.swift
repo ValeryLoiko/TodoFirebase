c692a2704b26f362c6383f16c8ec0bb0e514f9ba
Valery Loiko <valery.loiko.ios@gmail.com>
2023-05-23 16:14:00 +0200

    install Firebase

--- ToDoFirebase/ToDoFirebase/LoginViewController.swift
+++ ToDoFirebase/ToDoFirebase/LoginViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var validLabel: UILabel!
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardDidHide(notification:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func keyboardDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyboardSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + keyboardSize.height)
+        
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+    }
+    
+    @objc private func keyboardDidHide(notification: Notification) {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
 
     @IBAction func loginAction(_ sender: UIButton) {
